37ab4503b4349f610be540422344cfbc624bf531
vaibhav goswami <vaibhavgoswami2021@gmail.com>
2021-04-24 23:37:14 +0530

    A C program to print smilly face on screen.

---
+++ let us c question/chapter_6_que_b.c
@@ -0,0 +1,15 @@
+// Write a program to fill the entire screen with a smiling face. The
+// smiling face has an ASCII value 1.
+
+
+#include<stdio.h>
+
+int main(){
+    char c=1;
+    for(int i=0; i<=10000;i++)
+    {
+    printf("%c ",c);
+
+    }
+    return 0;
+}
