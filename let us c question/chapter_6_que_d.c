75016edbc852510dd5bedf983dd8e0dd94abfd2d
vaibhav goswami <vaibhavgoswami2021@gmail.com>
2021-04-25 23:45:44 +0530

    A C program which gives all possible combinations of 1 , 2 ,3

---
+++ let us c question/chapter_6_que_d.c
@@ -0,0 +1,47 @@
+// Write a program to generate all combinations of 1, 2 and 3 using for
+// loop.
+              //  without repetition.
+
+
+// #include<stdio.h>  
+  
+// int main()  
+// {  
+//     int i, j, k;  
+  
+//     for(i = 1; i <= 3; i++)  
+//     {  
+//         for(j = 1; j <= 3; j++)  
+//         {  
+//             for(k = 1; k <= 3; k++)  
+//             {  
+//                 if( i != j && i != k && j != k)  
+//                     printf("%d %d %d\n", i, j, k);  
+//             }  
+//         }  
+//     }  
+  
+//     return 0;  
+// }  
+
+               // with repetition.
+
+// #include<stdio.h>  
+  
+// int main()  
+// {  
+//     int i, j, k;  
+  
+//     for(i = 1; i <= 3; i++)  
+//     {  
+//         for(j = 1; j <= 3; j++)  
+//         {  
+//             for(k = 1; k <= 3; k++)  
+//             {  
+//                     printf("%d %d %d\n", i, j, k);  
+//             }  
+//         }  
+//     }  
+  
+//     return 0;  
+// }  
